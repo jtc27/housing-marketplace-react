169c1c17213dd4b08481abfeaaa8087669081923
Jacob Cho <jacobcho@live.com>
2022-04-04 04:02:00 +0100

    user login complete

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom"
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
@@ -23,6 +25,22 @@ function SignIn() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+  
+      if (userCredential.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
     <div className="pageContainer">
@@ -32,7 +50,7 @@ function SignIn() {
 
       <main>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input type="email" className="emailInput" placeholder="Email" id="email" 
           value={email} //email from formData state
           onChange={onChange}
