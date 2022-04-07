172ffb48c3f82bf619a2f5d68722f0bd636d134a
Jacob Cho <jacobcho@live.com>
2022-04-07 13:04:28 +0100

    listings saved to firestore storage

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,29 +1,24 @@
-import { useState, useEffect, useRef } from "react"
-
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { 
-  getStorage, 
-  ref, 
-  uploadBytesResumable, 
-  getDownloadURL } from "firebase/storage"; //image upload
-import {db} from '../firebase.config'
-
-import {v4 as uuidv4} from 'uuid' //ids for images
-
-import { useNavigate } from "react-router-dom";
-
-import Spinner from "../components/Spinner";
-import { toast } from "react-toastify";
+import { useState, useEffect, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { v4 as uuidv4 } from 'uuid'
+import Spinner from '../components/Spinner'
 
 function CreateListing() {
-
+  // eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(true)
-  //enabled by default
-
   const [loading, setLoading] = useState(false)
-
   const [formData, setFormData] = useState({
-    type:'rent',
+    type: 'rent',
     name: '',
     bedrooms: 1,
     bathrooms: 1,
@@ -35,10 +30,9 @@ function CreateListing() {
     discountedPrice: 0,
     images: {},
     latitude: 0,
-    longitude: 0
+    longitude: 0,
   })
 
-  //destructure
   const {
     type,
     name,
@@ -57,115 +51,140 @@ function CreateListing() {
 
   const auth = getAuth()
   const navigate = useNavigate()
+  const isMounted = useRef(true)
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setFormData({
-          ...formData,
-          userRef: user.uid
-        })
-      } else {
-        navigate('/sign-in')
-      }
-    })
-  }, [])
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid })
+        } else {
+          navigate('/sign-in')
+        }
+      })
+    }
+
+    return () => {
+      isMounted.current = false
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMounted])
 
   const onSubmit = async (e) => {
     e.preventDefault()
 
     setLoading(true)
-    
-    if(discountedPrice >= regularPrice) {
+
+    if (discountedPrice >= regularPrice) {
       setLoading(false)
-      toast.error('Discounted Price cannot be greater than Regular Price')
+      toast.error('Discounted price needs to be less than regular price')
       return
     }
 
-    if(images.length > 6) {
+    if (images.length > 6) {
       setLoading(false)
-      toast.error('You are allow a maximum of 6 images')
+      toast.error('Max 6 images')
+      return
     }
 
     let geolocation = {}
-    let location 
+    let location
 
-    if(geolocationEnabled) {
-      //make req to Google
-
+    if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      )
 
       const data = await response.json()
 
-      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0 //gets the lat and long from API
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
       geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
-      location = data.status === 'ZERO_RESULTS' //checks if address is legit
-        ? undefined
-        : data.results[0]?.formatted_address
 
-        if (location === undefined || location.includes('undefined')) {
-          setLoading(false)
-          toast.error('Please enter a correct address')
-          return
-        }
+      location =
+        data.status === 'ZERO_RESULTS'
+          ? undefined
+          : data.results[0]?.formatted_address
+
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false)
+        toast.error('Please enter a correct address')
+        return
+      }
     } else {
-      geolocation.lat = latitude  //gets the lat and long from the form inputs
-      geolocation.lgn = longitude
-      location = address
+      geolocation.lat = latitude
+      geolocation.lng = longitude
     }
 
-    //Store image in firebase
+    // Store image in firebase
     const storeImage = async (image) => {
       return new Promise((resolve, reject) => {
-        const storage = getStorage() //init storage
-        const fileName = `${auth.currentUser.displayName}-
-        ${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        const storage = getStorage()
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
 
         const storageRef = ref(storage, 'images/' + fileName)
 
         const uploadTask = uploadBytesResumable(storageRef, image)
 
-        uploadTask.on('state_changed', 
-        (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused');
-              break;
-            case 'running':
-              console.log('Upload is running');
-              break;
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            console.log('Upload is ' + progress + '% done')
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused')
+                break
+              case 'running':
+                console.log('Upload is running')
+                break
+              default:
+                break
+            }
+          },
+          (error) => {
+            reject(error)
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL)
+            })
           }
-        }, 
-        (error) => {
-          reject(error)  //if our Promise fails reject
-        }, 
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL);
-            // our Promise works and we resolve the array of images
-          });
-        }
-      );
+        )
       })
     }
 
-    const imgUrls = await Promise.all(   //storeImage() returns Promises.  Promise.all resolves multiple Promises
+    const imgUrls = await Promise.all(
       [...images].map((image) => storeImage(image))
     ).catch(() => {
       setLoading(false)
-      toast.error('Image is not uploaded')
+      toast.error('Images not uploaded')
       return
     })
 
-    console.log(imgUrls);
+    //formData + extras
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    }
 
+    //edits before submission
+    formDataCopy.location = address
+    delete formDataCopy.images
+    delete formDataCopy.address
+    !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+    //submission
+    const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
     setLoading(false)
-
+    toast.success('New listing has been created')
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`) //redirects to newly created listing
   }
-
+  
   const onMutate = (e) => {
     let boolean = null
 
@@ -234,7 +253,7 @@ function CreateListing() {
 
           </div>
 
-          <label className='formLabel'>Name</label>
+          <label className='formLabel'>Name of Property</label>
           <input
             className='formInputName'
             type='text'
