3005192ef13fcbe2e2e0db2bd1d9144fb1074692
Jacob Cho <jacobcho@live.com>
2022-04-08 11:18:07 +0100

    contact landlord

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignUp from "./pages/SignUp";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
@@ -30,6 +31,7 @@ function App() {
         </Route>
 
         <Route path='/create-listing' element={<CreateListing/>} />
+        <Route path='/contact/:landlordId' element={<Contact/>} />
 
         <Route path='/category/:categoryName/:listingId' element={<Listing/>} />
 
