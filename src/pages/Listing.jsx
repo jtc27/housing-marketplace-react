52dfb84f91d755eb29760f932f4a48644937e6d5
Jacob Cho <jacobcho@live.com>
2022-04-11 12:21:09 +0100

    swiper library with photos of house

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,6 +5,10 @@ import { Link, useNavigate, useParams } from "react-router-dom"
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 // For the leaflet map
 
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y} from 'swiper'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+
 import { doc, getDoc } from "firebase/firestore"
 
 import { getAuth } from "firebase/auth"
@@ -13,6 +17,7 @@ import { db } from "../firebase.config"
 import Spinner from "../components/Spinner"
 import shareIcon from '../assets/svg/shareIcon.svg'
 
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState(null)
@@ -43,7 +48,20 @@ function Listing() {
 
   return (
     <main>
-      {/* slider */}
+      <Swiper slidesPerView={1} pagination={{clickable: true}}>
+        //loop through images
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div className="swiperSlideDiv" 
+            style={{
+              background: `url(${listing.imgUrls[index]}) center no-repeat`,
+              backgroundSize: 'cover'
+            }}>
+              
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div className="shareIconDiv" onClick={() => {
         navigator.clipboard.writeText(window.location.href)
